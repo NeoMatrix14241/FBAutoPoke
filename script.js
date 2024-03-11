56bc25386a546ca31567042655c8b7de55771857
Kyle Capistrano <73865948+NeoMatrix14241@users.noreply.github.com>
2024-03-11 12:41:02 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function jsonColumnToParagraph(jsonData, display) {
 fetch('lib.json')
 	.then(response => response.json())
 	.then(data => {
-		var paragraphElement = document.getElementById("myParagraph");
+		var paragraphElement = document.getElementById("content");
 		paragraphElement.innerHTML = jsonColumnToParagraph(data, display);
 		console.log("working");
 	})
